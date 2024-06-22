30b69ba1c84236dc0ff00f1100c4f124fc8855b9
oeun42 <oeunzzang@naver.com>
2024-06-22 16:34:54 +0900

    init

---
+++ src/main/java/com/example/ssologin/SsologinApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsologinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsologinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssologin/SsologinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsologinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
